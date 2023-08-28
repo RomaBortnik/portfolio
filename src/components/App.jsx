569a59cf0f5be37f08ab8b5847b161fd0c44d01b
RomaBortnik <bortnik25051998@gmail.com>
2023-08-28 17:55:12 +0300

    added fonts and animations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,26 @@ import { TypeAnimation } from 'react-type-animation';
 
 export const App = () => {
   return (
-    <div>
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
       <TypeAnimation
         sequence={[
-          "I'm a Developer",
+          "I'm a Romko",
           1000,
-          "I'm a Writer",
+          "I'm a Kotichok",
           1000,
-          "I'm a Designer",
+          "I'm a Kotuuuuzyk",
           1000,
         ]}
-        speed={50}
+        speed={10}
         repeat={Infinity}
-        style={{ fontSize: '2em' }}
+        style={{ fontSize: 48 }}
       />
     </div>
   );
