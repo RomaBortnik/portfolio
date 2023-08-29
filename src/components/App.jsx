cb27dab6cd5c23050b3e8aa3df0f417992df8a3a
RomaBortnik <bortnik25051998@gmail.com>
2023-08-29 11:35:17 +0300

    added animations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,49 @@
 import { TypeAnimation } from 'react-type-animation';
+import Lottie from 'lottie-react';
+import animation from './Animation.json';
+import Reveal from './Reveal';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-      }}
-    >
-      <TypeAnimation
-        sequence={[
-          "I'm a Romko",
-          1000,
-          "I'm a Kotichok",
-          1000,
-          "I'm a Kotuuuuzyk",
-          1000,
-        ]}
-        speed={10}
-        repeat={Infinity}
-        style={{ fontSize: 48 }}
-      />
-    </div>
+    <Reveal>
+      <div
+        style={{
+          display: 'flex',
+          paddingLeft: 120,
+        }}
+      >
+        <div style={{}}>
+          <p style={{ fontSize: '64px', marginTop: 200 }}>Hello, Kotuuuuuzka</p>
+          <TypeAnimation
+            sequence={[
+              "I'm Romko",
+              1000,
+              "I'm Kotichok",
+              1000,
+              "I'm Kotuuuuzyk",
+              1000,
+              "I'm murko-burko",
+              1000,
+              "I'm Kotuskin",
+              1000,
+              "I'm vovtuuuuzyyyyyk",
+              1000,
+            ]}
+            speed={10}
+            repeat={Infinity}
+            style={{ fontSize: 48 }}
+          />
+        </div>
+        <Lottie
+          style={{
+            position: 'absolute',
+            right: 100,
+            width: 600,
+          }}
+          animationData={animation}
+          loop={true}
+        />
+      </div>
+    </Reveal>
   );
 };
