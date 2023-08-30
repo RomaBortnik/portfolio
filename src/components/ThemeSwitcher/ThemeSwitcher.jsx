06f1dc8825b23814922fdc743649e029fab94a9c
RomaBortnik <bortnik25051998@gmail.com>
2023-08-30 09:11:33 +0300

    added icons for themeSwitcher

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -14,10 +14,10 @@ const ThemeSwitcher = () => {
       <motion.div layout className="handle">
         <AnimatePresence mode="wait" initial={false}>
           <motion.i
-            className={`icon far fa-${isOn ? 'moon' : 'sun'}`}
+            className={`icon fi-rr-${isOn ? 'moon' : 'sun'}`}
             key={isOn ? 'moon' : 'sun'}
             initial={{ y: -30, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
+            animate={{ y: 2, opacity: 1 }}
             exit={{ y: 30, opacity: 0 }}
             transition={{ duration: 0.2 }}
           ></motion.i>
