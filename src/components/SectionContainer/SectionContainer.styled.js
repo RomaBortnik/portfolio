0ea8c9bc10736bc74028872a3fee4d37bd45758e
RomaBortnik <bortnik25051998@gmail.com>
2023-09-08 17:04:58 +0300

    changes

--- src/components/SectionContainer/SectionContainer.styled.js
+++ src/components/SectionContainer/SectionContainer.styled.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 import { sizes, devices } from 'styles';
 
 export const Container = styled.div`
-  padding: 0 12px;
+  padding: 0 4px;
   margin: 0 auto;
 
   @media screen and (${devices.mobile}) {
@@ -11,9 +11,11 @@ export const Container = styled.div`
 
   @media screen and (${devices.tablet}) {
     width: ${sizes.tablet};
+    padding: 0 8px;
   }
 
   @media screen and (${devices.desktop}) {
     width: ${sizes.desktop};
+    padding: 0 12px;
   }
 `;
--- src/components/SectionProjects/ProjectItem/ProjectItem.jsx
+++ src/components/SectionProjects/ProjectItem/ProjectItem.jsx
@@ -1,4 +1,4 @@
-import Reveal from 'components/Reveal';
+// import Reveal from 'components/Reveal';
 import LinkListItem from '../LinkListItem';
 import {
   Project,
@@ -13,21 +13,21 @@ const ProjectItem = ({ project }) => {
   const { id, name, image, altText, descr, media } = project;
   return (
     <Project>
-      <Reveal>
-        <ProjectThumb className="thumb">
-          <img src={image} alt={altText} />
-        </ProjectThumb>
+      {/* <Reveal> */}
+      <ProjectThumb className="thumb">
+        <img style={{ height: '100%' }} src={image} alt={altText} />
+      </ProjectThumb>
 
-        <ProjectOverlay className="overlay">
-          <ProjectTitle>{name}</ProjectTitle>
-          <ProjectDescr>{descr}</ProjectDescr>
-          <LinkList>
-            {media.map(({ link, icon }) => (
-              <LinkListItem key={id} link={link} icon={icon} />
-            ))}
-          </LinkList>
-        </ProjectOverlay>
-      </Reveal>
+      <ProjectOverlay className="overlay">
+        <ProjectTitle>{name}</ProjectTitle>
+        <ProjectDescr>{descr}</ProjectDescr>
+        <LinkList>
+          {media.map(({ link, icon }) => (
+            <LinkListItem key={id} link={link} icon={icon} />
+          ))}
+        </LinkList>
+      </ProjectOverlay>
+      {/* </Reveal> */}
     </Project>
   );
 };
