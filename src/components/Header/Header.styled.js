d46a80fcb8846507fa1d777a44f3ddaf08696e53
RomaBortnik <bortnik25051998@gmail.com>
2023-09-12 13:41:25 +0300

    changes

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -5,8 +5,9 @@ export const StyledHeader = styled.header`
   width: 100%;
   height: 60px;
   background-color: ${props => props.theme.colors.headerBackground};
-  display: grid;
+  display: flex;
   align-items: center;
+  justify-content: center;
   color: ${props => props.theme.colors.mainTextColor};
   box-shadow: ${props => props.theme.colors.mainShadow};
 
