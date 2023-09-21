03094204b1b414f97e46758d494ae461debb8a0c
RomaBortnik <bortnik25051998@gmail.com>
2023-09-21 10:24:47 +0300

    added loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,31 @@
 import Header from './Header';
-// import SectionEducation from './SectionEducation';
+import SectionEducation from './SectionEducation';
 import SectionSkills from './SectionSkills';
 import Hero from './Hero';
-// import SectionProjects from './SectionProjects';
+import SectionProjects from './SectionProjects';
 import ScrollTopButton from './ScrollTopButton';
 import { ThemeProvider } from '@emotion/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { theme } from 'styles';
 import { StyledWrapper } from './Wrapper/Wrapper.styled';
 import { STORAGE_KEY } from './ThemeSwitcher/ThemeSwitcher';
 import Particle from './Particle';
 import Footer from './Footer';
+import Loader from './Loader';
 
 export const App = () => {
+  const [isLoaderVisible, setIsLoaderVisible] = useState(true);
   const [currentTheme, setCurrentTheme] = useState(() => {
     const value = JSON.parse(localStorage.getItem(STORAGE_KEY));
     return value ? { ...theme[value] } : { ...theme.dark };
   });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaderVisible(false);
+    }, 800);
+    return () => clearTimeout(timer);
+  }, []);
 
   const handleTheme = value => {
     value === 'dark'
@@ -26,14 +35,15 @@ export const App = () => {
 
   return (
     <ThemeProvider theme={currentTheme}>
+      {/* {isLoaderVisible && <Loader />} */}
       <StyledWrapper>
         <Header onThemeChange={handleTheme} />
         <main>
           <Particle theme={currentTheme}></Particle>
           <Hero />
-          {/* <SectionEducation /> */}
+          <SectionEducation />
           <SectionSkills />
-          {/* <SectionProjects /> */}
+          <SectionProjects />
           <ScrollTopButton />
         </main>
         <Footer />
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,29 @@
+import { BallTriangle } from 'react-loader-spinner';
+
+const Loader = () => {
+  return (
+    <BallTriangle
+      height={100}
+      width={100}
+      radius={5}
+      color="#4fa94d"
+      ariaLabel="ball-triangle-loading"
+      wrapperClass={{}}
+      wrapperStyle={{
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'grey',
+        zIndex: '1200',
+      }}
+      visible={true}
+    />
+  );
+};
+
+export default Loader;
