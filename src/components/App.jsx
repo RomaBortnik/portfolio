292db0b3dea76ebe1759aa453841df3736e549fa
RomaBortnik <bortnik25051998@gmail.com>
2023-09-21 17:36:37 +0300

    added Poppins font

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,27 +5,18 @@ import Hero from './Hero';
 import SectionProjects from './SectionProjects';
 import ScrollTopButton from './ScrollTopButton';
 import { ThemeProvider } from '@emotion/react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { theme } from 'styles';
 import { StyledWrapper } from './Wrapper/Wrapper.styled';
 import { STORAGE_KEY } from './ThemeSwitcher/ThemeSwitcher';
 import Particle from './Particle';
 import Footer from './Footer';
-import Loader from './Loader';
 
 export const App = () => {
-  const [isLoaderVisible, setIsLoaderVisible] = useState(true);
   const [currentTheme, setCurrentTheme] = useState(() => {
     const value = JSON.parse(localStorage.getItem(STORAGE_KEY));
     return value ? { ...theme[value] } : { ...theme.dark };
   });
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoaderVisible(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
 
   const handleTheme = value => {
     value === 'dark'
@@ -35,22 +26,18 @@ export const App = () => {
 
   return (
     <ThemeProvider theme={currentTheme}>
-      {isLoaderVisible ? (
-        <Loader />
-      ) : (
-        <StyledWrapper>
-          <Header onThemeChange={handleTheme} />
-          <main>
-            <Particle theme={currentTheme}></Particle>
-            <Hero />
-            <SectionEducation />
-            <SectionSkills />
-            <SectionProjects />
-            <ScrollTopButton />
-          </main>
-          <Footer />
-        </StyledWrapper>
-      )}
+      <StyledWrapper>
+        <Header onThemeChange={handleTheme} />
+        <main>
+          <Particle theme={currentTheme}></Particle>
+          <Hero />
+          <SectionEducation />
+          <SectionSkills />
+          <SectionProjects />
+          <ScrollTopButton />
+        </main>
+        <Footer />
+      </StyledWrapper>
     </ThemeProvider>
   );
 };
